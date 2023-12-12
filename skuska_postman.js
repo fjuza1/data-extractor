412b8c938ec1951157485feb18b2c857f53ba010
fjuza1 <104192489+fjuza1@users.noreply.github.com>
2023-12-12 10:38:19 +0100

    Update skuska_postman.js

--- skuska_postman.js
+++ skuska_postman.js
@@ -1,49 +1,57 @@
 'use strict'
 let response = {
-    name: "John Doe",
-    age: 30,
-    address: {
-      street: "123 Main St",
-      city: "Anytown",
-      country: "USA"
-    },
-    friends: [
-      {
-        friendNames: "Alice",
-        age: 28
-      },
-      {
-        friendNamez: "Bob",
-        age: 32
-      }
-    ],
-    meHow: [
-        {
-          fds: "Alice",
-          as: 28
-        },
-        {
-          dfgfdg: "Bob",
-          dfg: 32
-        }
-      ]
-  };
-const AutomatizaciaResReq = {
-    __ziskajObjektoveHodnoty(objekt){
-        return Object.values(objekt)
-    },
-   __ziskajDatovePolia(pole){
-    return this.__ziskajObjektoveHodnoty(pole)
-    .filter(datovyTyp=>Array.isArray(datovyTyp))
-    .reduce((arr,acc)=>arr.concat(acc),[])
-   }
+	name: "John Doe",
+	age: 30,
+	address: {
+		street: "123 Main St",
+		city: "Anytown",
+		country: "USA"
+	},
+	friends: [{
+			friendNames: "Alice",
+			age: 28
+		},
+		{
+			friendNamez: "Bob",
+			age: 32
+		}
+	],
+	meHow: [{
+			fds: "Alice",
+			as: 28
+		},
+		{
+			dfgfdg: "Bob",
+			dfg: 32
+		}
+	]
 };
-const extraktovanePolia = AutomatizaciaResReq.__ziskajDatovePolia(response)
-// console.log("🚀 ~ file: skuska_postman.js:41 ~ extraktovanePolia:", extraktovanePolia)
-extraktovanePolia.forEach(element => {
-  Object.entries(element).forEach(([arr,val])=>{
-    console.log(arr,val);
-    return (arr,val)
-});
-})
+const AutomatizaciaResReq = {
+	__ziskajObjektoveHodnoty(objekt) {
+		return Object.values(objekt)
+	},
+	__ziskajDatovePolia(pole) {
+		return this.__ziskajObjektoveHodnoty(pole)
+			.filter(datovyTyp => Array.isArray(datovyTyp))
+			.reduce((arr, acc) => arr.concat(acc), [])
+	},
+	__ziskajKlucHodnotuZArr(datovePolia) {
+		this.__ziskajDatovePolia(datovePolia)
+			.forEach(element => {
+				if (Array.isArray(element)) return;
+				Object.entries(element).forEach(([key, val]) => {
+					if (Array.isArray(key) && Array.isArray(val)) return
+					console.log(key, val);
+				});
+			})
+	},
+  ziskajNedatovePolia (){
 
+  }
+};
+// AutomatizaciaResReq.__ziskajKlucHodnotuZArr(response)
+// let skuska = {
+//   __ziskajKlucHodnotuZArr:AutomatizaciaResReq.__ziskajObjektoveHodnoty
+// }
+// console.dir(skuska.__ziskajKlucHodnotuZArr(response)
+//   )
