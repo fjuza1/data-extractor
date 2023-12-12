38621e64cb0f238143e8a5b4dcc0785c05001b18
fjuza1 <104192489+fjuza1@users.noreply.github.com>
2023-12-12 16:13:34 +0100

    mozno ale zatial nie

--- skuska_postman.js
+++ skuska_postman.js
@@ -30,26 +30,30 @@ const AutomatizaciaResReq = {
 	__ziskajObjektoveHodnoty(objekt) {
 		return Object.values(objekt)
 	},
+	__zaplnPole(){}
 	__ziskajDatovePolia(pole) {
 		return this.__ziskajObjektoveHodnoty(pole)
 			.filter(datovyTyp => Array.isArray(datovyTyp))
 			.reduce((arr, acc) => arr.concat(acc), [])
 	},
+	__spracovanieVstupnychDat(element){
+		return 	Object.entries(element).forEach(([key, val]) => {
+			if (Array.isArray(key) && Array.isArray(val)) return
+			console.log(key, val);
+		});
+	},
 	__ziskajKlucHodnotuZArr(datovePolia) {
 		this.__ziskajDatovePolia(datovePolia)
 			.forEach(element => {
 				if (Array.isArray(element)) return;
-				Object.entries(element).forEach(([key, val]) => {
-					if (Array.isArray(key) && Array.isArray(val)) return
-					console.log(key, val);
-				});
+				this.__spracovanieVstupnychDat(element)
 			})
 	},
   ziskajNedatovePolia (){
 
   }
 };
-// AutomatizaciaResReq.__ziskajKlucHodnotuZArr(response)
+AutomatizaciaResReq.__ziskajKlucHodnotuZArr(response)
 // let skuska = {
 //   __ziskajKlucHodnotuZArr:AutomatizaciaResReq.__ziskajObjektoveHodnoty
 // }
