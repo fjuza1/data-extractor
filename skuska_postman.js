c68bbd5f80edf4b5550170e0117a986bf540d4b7
fjuza1 <104192489+fjuza1@users.noreply.github.com>
2023-12-13 16:47:28 +0100

    Update skuska_postman.js

--- skuska_postman.js
+++ skuska_postman.js
@@ -1,70 +1,73 @@
 'use strict'
 let response = {
-	name: "John Doe",
-	age: 30,
-	address: {
-		street: "123 Main St",
-		city: "Anytown",
-		country: "USA"
-	},
-	person: {
-		firstName: "John",
-		lastName: "Doe",
-		age: 50,
-		eyeColor: "blue",
-		x:{type:"Fiat", model:"500", color:"white"},
-		address: {
-			street: "123 Main St",
-			city: "Anytown",
-			country: "USA",
-		},
-	},
-	friends: [{
-			friendNames: "Alice",
-			age: 28
-		},
-		{
-			friendNamez: "Bob",
-			age: 32
-		}
-	],
-	meHow: [{
-			fds: "Alice",
-			as: 28
-		},
-		{
-			dfgfdg: "Bob",
-			dfg: 32
-		}
-	]
+    name: "John Doe",
+    age: 30,
+    size: 'dss',
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        country: "USA"
+    },
+    person: {
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue",
+        x: {
+            type: "Fiat",
+            model: "500",
+            color: "white"
+        },
+        address: {
+            street: "123 Main St",
+            city: "Anytown",
+            country: "USA",
+        },
+    },
+    friends: [{
+            friendNames: "Alice",
+            age: 28
+        },
+        {
+            friendNamez: "Bob",
+            age: 32
+        }
+    ],
+    meHow: [{
+            fds: "Alice",
+            as: 28
+        },
+        {
+            dfgfdg: "Bob",
+            dfg: 32
+        }
+    ]
 };
 const AutomatizaciaResReq = {
-	__ziskajObjektoveHodnoty(objekt) {
-		return Object.values(objekt)
-	},
-	__ziskajObjektoveKluce(objekt) {
-		return Object.keys(objekt)
-	},
-	__ziskajVsetkyObjekty(res){
-			return this.__ziskajObjektoveHodnoty(res).map(v => v instanceof Object ? Object.values(v) : [v])
-			.reduce((acc, next) => acc.concat(...next), [])
-			.filter(obj=>typeof obj === 'object')
-	}
+    __ziskajObjektoveHodnoty(objekt) {
+        return Object.values(objekt)
+    },
+    __ziskajObjektoveKluce(objekt) {
+        return Object.keys(objekt)
+    },
+    __jePrimitivna(obj) {
+        return typeof obj !== 'object'
+    },
+    __primitivne(obj) {
+        let objekt = {};
+        this.__ziskajObjektoveKluce(obj)
+            .forEach(element => {
+                const key = obj[element];
+                this.__jePrimitivna(key) ? objekt[element] = key : false
+            })
+        return objekt
+    },
+    __ziskajVsetkyObjekty(res) {
+        return this.__ziskajObjektoveHodnoty(res).map(v => v instanceof Object ? Object.values(v) : [v])
+            .reduce((acc, next) => acc.concat(...next), [])
+            .filter(obj => typeof obj === 'object')
+    }
 };
 const vsetkyObjekty = AutomatizaciaResReq.__ziskajVsetkyObjekty(response)
-// console.log("🚀 ~ file: skuska_postman.js:54 ~ vsetkyObjekty:", vsetkyObjekty)
-/*
-console.log(Object.keys(response));
-for (const key in response) {
-	if (Object.hasOwnProperty.call(response, key)) {
-		const element = Object.values(response[key]).filter(item=>typeof item !== 'object' && !Array.isArray(item))
-		if(!element.length === []) return
-		console.log("🚀 ~ file: skuska_postman.js:59 ~ element:", element)
-	}
-}*/
-// console.log(Object.values(response).map(v => !(v instanceof Object)));
-
-const hasNonObjectValues = Object.values(response).map(v => !(v instanceof Object));
-console.log("🚀 ~ file: skuska_postman.js:68 ~ hasNonObjectValues:", hasNonObjectValues)
-
-  
+const result = AutomatizaciaResReq.__primitivne(response);
+console.log(result);
