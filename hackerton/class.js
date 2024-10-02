b65936ba3900e1e5d139cea9ef41756bcc9f5bd9
fjuza1 <fjuza@ditec.sk>
2024-10-02 11:51:26 +0200

    Create class.js

---
+++ hackerton/class.js
@@ -0,0 +1,12 @@
+class CityDesign {
+    _cityDesign(options,a,b){
+        return this._cityGridLayout(options.cityLayout,a,b);
+    }
+    _cityGridLayout(layout, a , b){
+        if(layout === 'rectangle' && b === undefined) return `Rectangle accepts width and height`
+        if (layout === 'square') return `${a ** 2} m²`
+        if (layout === 'rectangle') return `${a*b} m²`
+    }
+}
+const city  = new CityDesign();
+console.log(city._cityDesign({cityLayout: 'square'},5));
